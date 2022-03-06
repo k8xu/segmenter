b3e1c69b0ef7bd2eb08744430f35c3e711fe1c01
Anushka Ray <anushkaray101@gmail.com>
2022-03-06 19:04:12 +0000

    incorporated aerial drone dataset

---
+++ segm/data/aerial_drone.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from segm.data.base import BaseMMSeg
+from segm.data import utils
+from segm.config import dataset_dir
+
+
+AERIAL_CONFIG_PATH = Path(__file__).parent / "config" / "aerial_drone.py" #have to add this file in that location
+AERIAL_CATS_PATH = Path(__file__).parent / "config" / "aerial_drone.yml" #have to add this file in that location
+
+
+class AerialDroneDataset(BaseMMSeg):
+    def __init__(self, image_size, crop_size, split, **kwargs):
+        super().__init__(
+            image_size,
+            crop_size,
+            split,
+            AERIAL_CONFIG_PATH,
+            **kwargs,
+        )
+        self.names, self.colors = utils.dataset_cat_description(AERIAL_CATS_PATH)
+        self.n_cls = 150
+        self.ignore_label = 0
+        self.reduce_zero_label = True
+
+    def update_default_config(self, config):
+        root_dir = dataset_dir()
+        path = Path(root_dir) / "aerial_drone"
+        config.data_root = path
+        if self.split == "train":
+            config.data.train.data_root = path / "dataset" 
+        elif self.split == "trainval":
+            config.data.trainval.data_root = path / "datset" 
+        elif self.split == "val":
+            config.data.val.data_root = path / "dataset" 
+        elif self.split == "test":
+            config.data.test.data_root = path / "release_test" 
+        config = super().update_default_config(config)
+        return config
+
+    def test_post_process(self, labels):
+        return labels + 1
